6b39f35fd8972e63eb22ef4bf9685d01afdafc1b
Vincenzo Navarra <54999963+vinnav@users.noreply.github.com>
2020-09-02 18:18:05 +0200

    add grid style and foreign

--- script.js
+++ script.js
@@ -6,17 +6,23 @@ let peso = document.getElementById('peso').value
 let pesoVolumetricoSDA = 0
 let pesoVolumetricoTNT = 0
 let pesoVolumetricoNP = 0
+let summary = document.getElementById('summary');
+document.getElementById("addPack").addEventListener("click", fill_colli)
 function submitted(){
     let altezza = document.getElementById('altezza').value
     let lunghezza = document.getElementById('lunghezza').value
     let larghezza = document.getElementById('larghezza').value
+    let country = document.getElementById('inputDa').value
     peso = document.getElementById('peso').value
     pesoVolumetricoSDA = altezza*lunghezza*larghezza/3333
     pesoVolumetricoTNT = altezza/100*lunghezza/100*larghezza/100*250
     pesoVolumetricoNP = altezza*lunghezza*larghezza/5000
     console.log("Il peso SDA é: " + pesoVolumetricoSDA + " Il peso TNT é: " + pesoVolumetricoTNT + " Il peso NP é: " + pesoVolumetricoNP);
+    console.log("Il paese é: " + country)
     peso_volume()
     calcolo_prezzi()
+    document.getElementById("dati").style.visibility = "hidden";
+    document.getElementById("priceTable").style.visibility = "visible";
 }
 
 let pesoSDA = 0
@@ -60,17 +66,17 @@ function calcolo_prezzi(){
     } else if(pesoSDA > 2 && pesoSDA <= 5){
         costSDA = 9.50
     } else if(pesoSDA > 5 && pesoSDA <= 10){
-        costSDA = 12.50
+        costSDA = 13
     } else if(pesoSDA > 10 && pesoSDA <= 20){
         costSDA = 15
     } else if(pesoSDA > 20 && pesoSDA <= 30){
-        costSDA = 18
+        costSDA = 18.50
     } else if(pesoSDA > 30 && pesoSDA <= 50){
-        costSDA = 28
+        costSDA = 30
     } else if(pesoSDA > 50 && pesoSDA <= 70){
-        costSDA = 38
+        costSDA = 40
     } else if(pesoSDA > 70 && pesoSDA <= 100){
-        costSDA = 48
+        costSDA = 50
     }
     console.log("Costo SDA: " + costSDA)
     // TNT
@@ -117,23 +123,23 @@ function calcolo_prezzi(){
     console.log("Costo NP: " + costNP)
     // SDA Personal
     if(pesoSDA <= 2){
-        costSDAPersonal = 6.30
+        costSDAPersonal = 6.53
     } else if(pesoSDA > 2 && pesoSDA <= 5){
-        costSDAPersonal = 7.30
+        costSDAPersonal = 7.60
     } else if(pesoSDA > 5 && pesoSDA <= 10){
-        costSDAPersonal = 8.60
+        costSDAPersonal = 8.95
     } else if(pesoSDA > 10 && pesoSDA <= 20){
-        costSDAPersonal = 10.40
+        costSDAPersonal = 10.77
     } else if(pesoSDA > 20 && pesoSDA <= 30){
-        costSDAPersonal = 12.70
+        costSDAPersonal = 13.20
     } else if(pesoSDA > 30 && pesoSDA <= 50){
-        costSDAPersonal = 16.50
+        costSDAPersonal = 17.10
     } else if(pesoSDA > 50 && pesoSDA <= 70){
-        costSDAPersonal = 22.50
+        costSDAPersonal = 23.40
     } else if(pesoSDA > 70 && pesoSDA <= 100){
-        costSDAPersonal = 30
+        costSDAPersonal = 31.20
     }
-    console.log("Costo Personal: " + costSDAPersonal)
+    console.log("Costo SDA Personal: " + costSDAPersonal)
     // TNT Personal
     if(pesoTNT <= 1){
         costTNTPersonal = 8.60
@@ -182,4 +188,204 @@ function calcolo_prezzi(){
 
 }
 
-// Foreign deliveries
+function fill_colli(){
+    let altezza = document.getElementById('altezza').value
+    let lunghezza = document.getElementById('lunghezza').value
+    let larghezza = document.getElementById('larghezza').value
+    let country = document.getElementById('inputDa').value
+    let peso = document.getElementById('peso').value
+    summary.innerHTML += "<p>Collo " + altezza + "x" + lunghezza + "x" + larghezza + ", peso: " + peso + "</p>"
+}
+// Foreign deliverie" ffffffffffffffffff"
+"afghanistan"
+"albania"
+"algeria"
+"andorra"
+"angola"
+"antiguaEBarbuda"
+"arabiaSaudita"
+"argentina"
+"armenia"
+"australia"
+"austria"
+"azerbaigian"
+"bahamas"
+"bahrein"
+"bangladesh"
+"barbados"
+"belgio"
+"belize"
+"benin"
+"bhutan"
+"bielorussia"
+"birmania"
+"bolivia"
+"bosniaEdErzegovinaegovina"
+"botswana"
+"brasile"
+"brunei"
+"bulgaria"
+"burkinaFaso"
+"burundi"
+"cambogia"
+"camerun"
+"canada"
+"capoVerde"
+"ciad"
+"cile"
+"cina"
+"cipro"
+"colombia"
+"comore"
+"repDelCongo"
+"rdDelCongo"
+"coreaDelNord"
+"coreaDelSud"
+"costaDAvorio"
+"croazia"
+"cuba"
+"danimarca"
+"dominica"
+"ecuador"
+"egitto"
+"elSalvador"
+"emiratiArabiUniti"
+"eritrea"
+"estonia"
+"etiopia"
+"figi"
+"filippine"
+"finlandia"
+"francia"
+"gabon"
+"georgia"
+"germania"
+"ghana"
+"giamaica"
+"giappone"
+"gibuti"
+"giordania"
+"grecia"
+"grenada"
+"guatemala"
+"guinea"
+"guineaBissau"
+"guineaEquatoriale"
+"guyana"
+"haiti"
+"honduras"
+"india"
+"indonesia"
+"iran"
+"iraq"
+"irlanda"
+"islanda"
+"isoleMarshall"
+"isoleSolomone"
+"israele"
+"Italia"
+"kazakistan"
+"kenya"
+"kirghizistan"
+"kuwait"
+"laos"
+"lesotho"
+"lettonia"
+"libano"
+"liberia"
+"libia"
+"liechtenstein"
+"lituania"
+"lussemburgo"
+"macedoniaDelNord"
+"madagascar"
+"malawi"
+"maldive"
+"malaysia"
+"mali"
+"malta"
+"marocco"
+"mauritania"
+"mauritius"
+"messico"
+"micronesia"
+"moldavia"
+"monaco"
+"mongolia"
+"montenegro"
+"mozambico"
+"namibia"
+"nauru"
+"nepal"
+"nicaragua"
+"niger"
+"nigeria"
+"norvegia"
+"nuovaZelanda"
+"oman"
+"paesiBassi"
+"pakistan"
+"palau"
+"palestina"
+"panama"
+"papuaNuovaGuinea"
+"paraguay"
+"peru"
+"polonia"
+"portogallo"
+"qatar"
+"regnoUnito"
+"repubblicaCeca"
+"repubblicaCentrafricana"
+"repubblicaDominicaDominicana"
+"romania"
+"ruanda"
+"russia"
+"saintKittsENevis"
+"saintLucia"
+"saintVincentEGrenadine"
+"samoa"
+"sanMarino"
+"saoTomeEPrincipe"
+"senegal"
+"serbia"
+"seychelles"
+"sierraLeone"
+"singapore"
+"siria"
+"slovacchia"
+"slovenia"
+"somalia"
+"spagna"
+"statiUniti"
+"sudafrica"
+"sudan"
+"sudanDelSud"
+"suriname"
+"svezia"
+"svizzera"
+"swaziland"
+"tagikistan"
+"taiwan"
+"tanzania"
+"thailandia"
+"timorEst"
+"togo"
+"tonga"
+"trinidadETobago"
+"tunisia"
+"turchia"
+"turkmenistan"
+"tuvalu"
+"ucraina"
+"uganda"
+"ungheria"
+"uruguay"
+"uzbekistan"
+"vanuatu"
+"cittaDelVaticano"
+"venezuela"
+"vietnam"
+"yemen"
+"zambia"
+"zimbabwe"
